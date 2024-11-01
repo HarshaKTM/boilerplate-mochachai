663f9731ff6358132e8b7d928771aab8740b89d1
Harsha Kumarasingha <harshakumara1998030944@gmail.com>
2024-11-01 13:32:25 +0530

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -71,14 +71,14 @@ suite('Unit Tests', function () {
       assert.isAbove(Math.PI, 3);
       assert.isAtMost(1 - Math.random(), 1);
     });
-
-    // #9
+  // #9
     test('#isBelow, #isAtLeast', function () {
-      assert.isAtLeast('world'.length, 5);
-      assert.isBelow(2 * Math.random(), 2);
-      assert.isBelow(5 % 2, 2);
-      assert.isBelow(2 / 3, 1);
+      assert.isAtLeast('world'.length, 5);   // Should be correct
+      assert.isAtLeast(2 * Math.random(), 2);  // Adjust as needed
+      assert.isBelow(5 % 2, 2);              // Should be correct
+      assert.isBelow(2 / 3, 1);              // Should be correct
     });
+    
 
     // #10
     test('#approximately', function () {
